446d3aa7493954d8264d4843a12306e39b706697
Guicbdiniz <guicbdeniz@hotmail.com>
2020-05-01 23:48:29 -0300

    feat: react router added

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,12 +1,17 @@
 import React from 'react'
 import { DailyUpdateForm } from './components/DailyUpdateForm'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 
 function App() {
 	return (
-		<div className="App">
-			<h1> Quarantilog </h1>
-			<DailyUpdateForm />
-		</div>
+		<Router>
+			<div className="App">
+				<h1> Quarantilog </h1>
+				<Switch>
+					<Route path="/" component={DailyUpdateForm} />
+				</Switch>
+			</div>
+		</Router>
 	)
 }
 
